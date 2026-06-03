8eeb775f47e9cf697950cdc5ba4049293d6e0e4a
mdomagal <mdomagal@stud.elka.pw.edu.pl>
2026-06-03 21:30:25 +0200

    refactored asNumber to use std::visit

--- src/Value.cpp
+++ src/Value.cpp
@@ -1,27 +1,34 @@
 #include "Value.h"
 #include <stdexcept>
 
-std::variant<int, double> Value::asNumber() const {
-    if (const int* val = std::get_if<int>(&m_data)) {
-        return *val;
-    }
-    if (const double* val = std::get_if<double>(&m_data))
-        return *val;
-    if (const bool* val = std::get_if<bool>(&m_data))
-        return *val ? 1 : 0;
-    if (const std::string* val = std::get_if<std::string>(&m_data)) {
-        try {
-            if (val->find('.') != std::string::npos)
-                return std::stod(*val);
-            return std::stoi(*val);
-        } catch (std::invalid_argument& e) {
-            throw std::runtime_error("Cannot implicitly convert string '" 
-                    + *val + "' into a number type");
-        } catch (std::out_of_range& e) {
-            throw std::runtime_error("Cannot implicitly convert string '" 
-                    + *val + "' into a number type - value is out of range");
-        }
-    }
+namespace {
+    template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+}
 
-    throw std::runtime_error("Cannot convert null to a number");
+std::variant<int, double> Value::asNumber() const {
+    return std::visit<std::variant<int, double>>(overloaded{
+        [](int val) { return val; },
+
+        [](double val) { return val; },
+
+        [](bool val) { return val ? 1 : 0; },
+
+        [](const std::string& val) -> std::variant<int, double> { 
+            try {
+                if (val.find('.') != std::string::npos)
+                    return std::stod(val);
+                return std::stoi(val);
+            } catch (std::invalid_argument& e) {
+                throw std::runtime_error("Cannot implicitly convert string '" 
+                        + val + "' into a number type");
+            } catch (std::out_of_range& e) {
+                throw std::runtime_error("Cannot implicitly convert string '" 
+                        + val + "' into a number type - value is out of range");
+            }
+        },
+
+        [](std::monostate) -> std::variant<int, double> {
+            throw std::runtime_error("Cannot convert null to a number");
+        }
+    }, m_data);
 }
